ecd1604049d3b1d65523620090a25406b74fc052
SyedDevOps <talal.syed17@gmail.com>
2019-01-08 15:07:23 +0500

    Components & states(props only)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class ListItems extends Component{
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    const {name} = this.props;
+    return(
+      <ul> 
+        { name.map( (n, index)=> 
+        <li key={index}> {n} </li> ) }
+      </ul>
+    );
+  }
+}
+
+class App extends Component{
+  render() {
+  const names = ['Faisal', 'Talal', 'Asad', 'Daniyal', 'Ali'];
+  const names1 = ['Oranges', 'Apples', 'Mangoes'];
+    return(
+      <div>
+        <ListItems name={names}/>
+        <ListItems name={names1}/>  
       </div>
     );
   }
